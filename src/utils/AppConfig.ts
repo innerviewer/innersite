eeac98de6db7e8cacdd1f4065a3ee4ae70914be1
innerviewer <innerviewerr@gmail.com>
2024-08-13 19:21:10 +0200

    (Git) Initial commit.

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+	site_name: 'InnerBlog',
+	title: 'InnerBlog',
+	description: "innerviewer's blog", 
+	author: 'innerviewer',
+	locale_region: 'en-en',
+	locale: 'en'
+};
